62d215927a429179f155fbf9540db3850fedcb48
a7luong7 <samcheung992@gmail.com>
2021-09-29 18:06:47 -0700

    added 404 catchall for all undefined routes

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,9 @@ if (process.env.NODE_ENV !== 'production') { app.use(cors()); }
 app.use('/api/songs', songsRouter);
 app.use('/api/albums', albumsRouter);
 
+// 404 catchall
+app.get('*', (req, res) => {
+  res.status(404).json({ error: 'Invalid route' });
+});
+
 app.listen(process.env.PORT, () => {});
